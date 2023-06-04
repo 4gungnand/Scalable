80b06226647bb60ae3d009fd0a2de4a199fe8430
Premaaa <premananda2003@gmail.com>
2023-06-04 17:12:10 +0700

    Udah migrain, pengen tidur

--- app_plisbener/upload/forms.py
+++ app_plisbener/upload/forms.py
@@ -10,3 +10,15 @@ class GeeksForm(forms.ModelForm):
     class Meta:
         model = photos
         fields = "__all__"
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if ' ' in title:
+            raise forms.ValidationError("Title cannot contain spaces.")
+        return title
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        if ' ' in image.name:
+            raise forms.ValidationError("Filename cannot contain spaces.")
+        return image
--- app_plisbener/upload/views.py
+++ app_plisbener/upload/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import GeeksForm
 from .models import photos
 from urllib.parse import urljoin
+from django.conf import settings
+import cloudinary
+import requests
 
 def index(request):
     context = {}
@@ -9,26 +12,71 @@ def index(request):
     if request.method == 'POST':
         form = GeeksForm(request.POST, request.FILES)
         if form.is_valid():
-            image = form.save()
-            return redirect('success') 
+            title = form.cleaned_data['title']
+            image = form.cleaned_data['image']
+
+            # Save the file with the desired title
+            uploaded_image = cloudinary.uploader.upload(
+                image,
+                public_id=title,  # Set the public ID to the title
+                folder='scalable/'  # Optional: Set a folder to organize the images
+            )
+
+            # Handle the Cloudinary response and redirect to the success page
+            if 'public_id' in uploaded_image:
+                return redirect('success')  # Replace 'success' with your success page URL
+
     else:
         form = GeeksForm()
+
     context['form'] = form
-    
     return render(request, 'index.html', context)
 
 def success(request):
     context = {}
-
-    photo = photos.objects.latest('id')
-
-    url_full = photo.image.url
-    img_name = url_full.split('/')[-1]
-    base_url = "https://res.cloudinary.com/prema-cloud/image/upload/c_fill,h_150/"
-
-    new_url = urljoin(base_url, img_name)
-
-    context['photo'] = photo 
-    context['img_url'] = new_url
+    url = get_newest_image()
+    img_name = url.split('/')[-1]
+    context['img_url'] = url
+    context['img_name'] = img_name
+    # context['img_url'] = new_url
 
     return render(request, 'success.html', context)
+
+
+def get_newest_image():
+    cloudinary.config()
+
+    # Get the Cloudinary credentials from the configuration
+    cloud_name = cloudinary.config().cloud_name
+    api_key = cloudinary.config().api_key
+    api_secret = cloudinary.config().api_secret
+
+    # API endpoint
+    url = f'https://api.cloudinary.com/v1_1/{cloud_name}/resources/image/upload'
+
+    # Request parameters
+    params = {
+        'type': 'upload',
+        'max_results': 1,  # Number of images to retrieve
+        'sort_by': 'created_at',  # Sort by creation date
+        'direction': 'desc',  # Sort in descending order
+    }
+
+    # Make the API request
+    response = requests.get(url, auth=(api_key, api_secret), params=params)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Extract the public ID of the newest image 
+        if 'resources' in data and len(data['resources']) > 0:
+            newest_image_public_id = data['resources'][0]['public_id']
+            
+            # Construct the URL of the newest image
+            newest_image_url = f'https://res.cloudinary.com/{cloud_name}/image/upload/{newest_image_public_id}'
+            
+            return newest_image_url
+    
+    # Return None if the request fails or no images are found
+    return None
